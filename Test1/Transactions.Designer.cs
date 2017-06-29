b74a0256356f189fadc0a6b1fa09f04369c58f5b
RamezToma <ramez.mamdouh@live.com>
2017-06-29 23:39:31 +0200

    some editing in transactions

--- Test1/Transactions.Designer.cs
+++ Test1/Transactions.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -44,22 +46,38 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 139);
+            this.button2.Location = new System.Drawing.Point(12, 437);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(167, 52);
+            this.button2.Size = new System.Drawing.Size(167, 59);
             this.button2.TabIndex = 1;
             this.button2.Text = "Return";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(205, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(727, 484);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.AllowUserToOrderColumnsChanged += new System.EventHandler(this.datasell);
             // 
             // Transactions
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(750, 528);
+            this.ClientSize = new System.Drawing.Size(944, 528);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Transactions";
             this.Text = "Transactions";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -68,5 +86,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Test1/Transactions.cs
+++ Test1/Transactions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,83 @@ namespace Test1
 {
     public partial class Transactions : Form
     {
+        Test1.Utilities util = new Test1.Utilities();
+
+        SqlConnection conn;
+        DataTable dt = new DataTable();
+        SqlDataAdapter adap;
+
         public Transactions()
         {
             InitializeComponent();
+            Getdatagrid();
+          
+        }
+
+    
+
+
+        
+        private void Getdatagrid()
+        {
+            conn = new SqlConnection(util.GetConnectionString());
+
+            string query = "Select * From Transactions";
+
+            try
+            {
+                conn.Open();
+                adap = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                adap.Fill(dt);
+                dataGridView1.DataSource = dt;
+                
+                conn.Close();
+
+            }
+            catch
+            {
+
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+           
             New_Transaction nt = new New_Transaction();
             nt.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //conn = new SqlConnection(util.GetConnectionString());
+
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                string value1 = row.Cells[0].Value.ToString();
+                string value2 = row.Cells[1].Value.ToString();
+                string value3 = row.Cells[2].Value.ToString();
+                string value4 = row.Cells[3].Value.ToString();
+                string value5 = row.Cells[4].Value.ToString();
+                string value6 = row.Cells[5].Value.ToString();
+                string value7 = row.Cells[6].Value.ToString();
+                string value8 = row.Cells[7].Value.ToString();
+
+                MessageBox.Show(value1+"    "+ value2 + "    " + value3 + "    " + value4 + "    " + value5 + "    " + value6 + "    " + value7 + "    " + value8 + "    "  );
+            }
+
+        }
+
+      
+
+        private void datasell(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                string value1 = row.Cells[1].Value.ToString();
+              
+               // MessageBox.Show(value1);
+            }
         }
     }
 }
