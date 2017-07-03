a0f73bedc4ce6f21f3bfefeb5eaa95cf87ae9915
RamezToma <ramez.mamdouh@live.com>
2017-07-03 22:22:01 +0200

    Item edited

--- Test1/Items.Designer.cs
+++ Test1/Items.Designer.cs
@@ -28,59 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataListView1 = new BrightIdeasSoftware.DataListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.code = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.name = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.supplier = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.type = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.qty = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.qtyunit = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.price = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataListView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dataListView1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(868, 463);
-            this.dataGridView1.TabIndex = 0;
+            this.dataListView1.AllColumns.Add(this.code);
+            this.dataListView1.AllColumns.Add(this.name);
+            this.dataListView1.AllColumns.Add(this.supplier);
+            this.dataListView1.AllColumns.Add(this.type);
+            this.dataListView1.AllColumns.Add(this.qty);
+            this.dataListView1.AllColumns.Add(this.qtyunit);
+            this.dataListView1.AllColumns.Add(this.price);
+            this.dataListView1.CellEditUseWholeCell = false;
+            this.dataListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.code,
+            this.name,
+            this.supplier,
+            this.type,
+            this.qty,
+            this.qtyunit,
+            this.price});
+            this.dataListView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataListView1.DataSource = null;
+            this.dataListView1.Location = new System.Drawing.Point(12, 50);
+            this.dataListView1.Name = "dataListView1";
+            this.dataListView1.Size = new System.Drawing.Size(875, 334);
+            this.dataListView1.TabIndex = 0;
+            this.dataListView1.UseCompatibleStateImageBehavior = false;
+            this.dataListView1.View = System.Windows.Forms.View.Details;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // code
+            // 
+            this.code.AspectName = "code";
+            this.code.Text = "Code";
+            // 
+            // name
+            // 
+            this.name.AspectName = "name";
+            this.name.Text = "Item Name";
+            // 
+            // supplier
+            // 
+            this.supplier.AspectName = "supplier";
+            this.supplier.Text = "Supplier";
+            // 
+            // type
+            // 
+            this.type.AspectName = "type";
+            this.type.Text = "type";
+            // 
+            // qty
+            // 
+            this.qty.AspectName = "qty";
+            this.qty.Text = "Quantity";
+            // 
+            // qtyunit
+            // 
+            this.qtyunit.AspectName = "qtyunit";
+            this.qtyunit.Text = "Quantity per unit";
+            // 
+            // price
+            // 
+            this.price.AspectName = "price";
+            this.price.Text = "Price";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 481);
+            this.button1.Location = new System.Drawing.Point(12, 405);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(443, 40);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Update Database";
+            this.button1.Size = new System.Drawing.Size(84, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Refresh";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(110, 465);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(355, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(110, 506);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(355, 20);
+            this.textBox3.TabIndex = 4;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(461, 481);
+            this.button2.Location = new System.Drawing.Point(12, 505);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(419, 40);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Refresh";
+            this.button2.Size = new System.Drawing.Size(92, 21);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 465);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 20);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Items
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(892, 552);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataListView1);
             this.Name = "Items";
             this.Text = "Items";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.Items_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataListView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private BrightIdeasSoftware.DataListView dataListView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private BrightIdeasSoftware.OLVColumn code;
+        private BrightIdeasSoftware.OLVColumn name;
+        private BrightIdeasSoftware.OLVColumn supplier;
+        private BrightIdeasSoftware.OLVColumn type;
+        private BrightIdeasSoftware.OLVColumn qty;
+        private BrightIdeasSoftware.OLVColumn qtyunit;
+        private BrightIdeasSoftware.OLVColumn price;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
