184a764779fc7ce9ba9296766d649682da87b4eb
RamezToma <ramez.mamdouh@live.com>
2017-07-03 21:23:53 +0200

    Return transaction completed

--- Test1/returntran.Designer.cs
+++ Test1/returntran.Designer.cs
@@ -56,99 +56,88 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(102, 73);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(51, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "كود البيان";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(372, 73);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Location = new System.Drawing.Point(186, 38);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 25);
+            this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "كود العميل";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(872, 73);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Location = new System.Drawing.Point(436, 38);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 25);
+            this.label3.Size = new System.Drawing.Size(55, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "اسم العميل";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(1376, 73);
-            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Location = new System.Drawing.Point(688, 38);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(110, 25);
+            this.label4.Size = new System.Drawing.Size(70, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "مبيعات البيات ";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(24, 140);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Location = new System.Drawing.Point(12, 73);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(260, 31);
+            this.textBox1.Size = new System.Drawing.Size(132, 20);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(300, 140);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox2.Location = new System.Drawing.Point(150, 73);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(268, 31);
+            this.textBox2.Size = new System.Drawing.Size(136, 20);
             this.textBox2.TabIndex = 5;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(584, 140);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox3.Location = new System.Drawing.Point(292, 73);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(674, 31);
+            this.textBox3.Size = new System.Drawing.Size(339, 20);
             this.textBox3.TabIndex = 6;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(1274, 140);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox4.Location = new System.Drawing.Point(637, 73);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(326, 31);
+            this.textBox4.Size = new System.Drawing.Size(165, 20);
             this.textBox4.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(1714, 73);
-            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Location = new System.Drawing.Point(857, 38);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 25);
+            this.label5.Size = new System.Drawing.Size(38, 13);
             this.label5.TabIndex = 8;
             this.label5.Text = "التاريخ";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(1616, 140);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox5.Location = new System.Drawing.Point(808, 73);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(212, 31);
+            this.textBox5.Size = new System.Drawing.Size(108, 20);
             this.textBox5.TabIndex = 9;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(24, 938);
-            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Location = new System.Drawing.Point(12, 488);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(262, 87);
+            this.button1.Size = new System.Drawing.Size(131, 45);
             this.button1.TabIndex = 10;
             this.button1.Text = "بالكامل";
             this.button1.UseVisualStyleBackColor = true;
@@ -156,29 +145,27 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1274, 938);
-            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Location = new System.Drawing.Point(637, 488);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(262, 87);
+            this.button2.Size = new System.Drawing.Size(131, 45);
             this.button2.TabIndex = 11;
             this.button2.Text = "مرتجع بعض السلع";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(1498, 852);
-            this.textBox6.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox6.Location = new System.Drawing.Point(749, 443);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(330, 31);
+            this.textBox6.Size = new System.Drawing.Size(167, 20);
             this.textBox6.TabIndex = 13;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(1714, 802);
-            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Location = new System.Drawing.Point(857, 417);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(90, 25);
+            this.label6.Size = new System.Drawing.Size(57, 13);
             this.label6.TabIndex = 14;
             this.label6.Text = "مبلغ مرتجع";
             // 
@@ -210,13 +197,14 @@
             this.qtyufinal,
             this.supllierfinal});
             this.objectListView1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.objectListView1.Location = new System.Drawing.Point(720, 337);
-            this.objectListView1.Margin = new System.Windows.Forms.Padding(6);
+            this.objectListView1.Location = new System.Drawing.Point(360, 175);
             this.objectListView1.Name = "objectListView1";
-            this.objectListView1.Size = new System.Drawing.Size(1108, 414);
+            this.objectListView1.Size = new System.Drawing.Size(556, 217);
             this.objectListView1.TabIndex = 15;
             this.objectListView1.UseCompatibleStateImageBehavior = false;
             this.objectListView1.View = System.Windows.Forms.View.Details;
+            this.objectListView1.CellEditFinished += new BrightIdeasSoftware.CellEditEventHandler(this.listView1_CellEditFinished);
+            this.objectListView1.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.listView1_ItemChecked);
             // 
             // codefinal
             // 
@@ -249,9 +237,9 @@
             // 
             // returntran
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1856, 1048);
+            this.ClientSize = new System.Drawing.Size(951, 537);
             this.Controls.Add(this.objectListView1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox6);
@@ -267,7 +255,6 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "returntran";
             this.Text = "returntran";
             this.Load += new System.EventHandler(this.returntran_Load);
--- Test1/returntran.cs
+++ Test1/returntran.cs
@@ -12,11 +12,12 @@ using System.Windows.Forms;
 
 
 
-public struct itemcdeqty {
+public struct itemcdeqty
+{
     public string code;
     public string qty;
 
-    public itemcdeqty (string cde , string qt)
+    public itemcdeqty(string cde, string qt)
     {
         this.code = cde;
         this.qty = qt;
@@ -29,14 +30,18 @@ public struct itemdisp
     public string qtyfinal;
     public string qtyufinal;
     public string supplierfinal;
+    public bool x;
+    public string oldqty;
 
-    public itemdisp (string x1, string x2 , string x3 , string x4 , string x5)
+    public itemdisp(string x1, string x2, string x3, string x4, string x5, bool x6, string x7)
     {
         this.codefinal = x1;
         this.namefinal = x2;
         this.qtyfinal = x3;
         this.qtyufinal = x4;
         this.supplierfinal = x5;
+        this.x = x6;
+        this.oldqty = x7;
     }
 
 }
@@ -54,11 +59,11 @@ namespace Test1
         string stuff1;
         string buysell1;
         string clientname1;
-        
+
         //Object senderform;
 
-           List <itemcdeqty> itemx = new List <itemcdeqty>();
-          List<itemdisp> itemdisplay = new List<itemdisp>(); 
+        List<itemcdeqty> itemx = new List<itemcdeqty>();
+        List<itemdisp> itemdisplay = new List<itemdisp>();
 
         public returntran(string x, string date, string clientid, string total, string stuff, string buysell, string clientname)
         {
@@ -69,16 +74,11 @@ namespace Test1
             stuff1 = stuff;
             buysell1 = buysell;
             clientname1 = clientname;
-
-          //  MessageBox.Show(x1 + date1);
-
-
-          
             InitializeComponent();
             getitems();
-            
+
         }
-     
+
 
         private void returntran_Load(object sender, EventArgs e)
         {
@@ -93,7 +93,7 @@ namespace Test1
             textBox5.Text = date1;
             textBox5.ReadOnly = true;
 
-            queryitem();   // loading query item to add items returned to olv
+            queryitem();
 
         }
 
@@ -115,33 +115,28 @@ namespace Test1
             while (stuff1 != "")
             {
 
-           //     MessageBox.Show(stuff1);
+
                 if (stuff1.IndexOf("&&++") >= 0)
-                {   
-                    
+                {
+
                     temp_code = stuff1.Substring(0, stuff1.IndexOf("&&++"));
                     stuff1 = stuff1.Substring(stuff1.IndexOf("&&++") + 4);
-         //           MessageBox.Show(stuff1);
+
                     if (stuff1.IndexOf("++&&") >= 0)
                     {
                         temp_qty = stuff1.Substring(0, stuff1.IndexOf("++&&"));
                         stuff1 = stuff1.Substring(stuff1.IndexOf("++&&") + 4);
-           //             MessageBox.Show(stuff1);
+
                     }
 
                     itemx.Add(new itemcdeqty(temp_code, temp_qty));
-                    
+
                 }
-              
+
             }
         }
 
-
-
-
-
-
-         void queryitem() // Get items from db and adding it to List <itemdisp> ..
+        void queryitem() // Get items from db and adding it to List <itemdisp> ..
         {
             Test1.Utilities util = new Utilities();
             SqlConnection conn = new SqlConnection(util.GetConnectionString());
@@ -150,13 +145,13 @@ namespace Test1
             SqlCommand cmdq = new SqlCommand(query, conn);
 
             SqlDataReader rdr;
-            
+
             objectListView1.CheckBoxes = true;
             objectListView1.View = View.Details;
             objectListView1.GridLines = true;
             objectListView1.FullRowSelect = true;
             objectListView1.AlwaysGroupBySortOrder = System.Windows.Forms.SortOrder.None;
-               
+
 
             try
             {
@@ -165,24 +160,24 @@ namespace Test1
 
                 while (rdr.Read())
                 {
-                    for (int i=0; i < itemx.Count; i++)
+                    for (int i = 0; i < itemx.Count; i++)
                     {
-                        if(itemx[i].code == rdr.GetString(0))
+                        if (itemx[i].code == rdr.GetString(0))
                         {
                             itemdisplay.Add(new itemdisp(rdr.GetString(0), rdr.GetString(1), itemx[i].qty,
-                                rdr.GetValue(5).ToString(), rdr.GetString(3)));
+                                rdr.GetValue(5).ToString(), rdr.GetString(3), false, "0"));
 
                             MessageBox.Show(itemx[i].qty);
                         }
-                       
+
                     }
 
 
-   
+
                 }
 
-               
-               
+
+
 
                 objectListView1.SetObjects(itemdisplay);
                 rdr.Close();
@@ -195,9 +190,99 @@ namespace Test1
             }
         }
 
-       
+        private void listView1_CellEditFinished(object sender, BrightIdeasSoftware.CellEditEventArgs e)
+        {
+            try
+            {
+                itemdisp tempitem = itemdisplay[objectListView1.SelectedIndex];
+                tempitem.oldqty = tempitem.qtyfinal;
+
+                if (e.Column.Text == "Quantity")
+                {
+                    string test = Convert.ToString(e.NewValue);
+                    if (Convert.ToInt32(test) > Convert.ToInt32(tempitem.oldqty))
+                    {
+                        MessageBox.Show("Quantity greater than initial sale");
+                        objectListView1.SetObjects(itemdisplay);
+                        return;
+                    }
+                    tempitem.qtyfinal = test;
+
+                    itemdisplay[objectListView1.SelectedIndex] = tempitem;
+                    itemdisplay[objectListView1.SelectedIndex] = tempitem;
+
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Editing error");
+            }
+        }
+
+
+        private void listView1_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+
+            itemdisp tempitem = itemdisplay[e.Item.Index];
+            tempitem.x = e.Item.Checked;
+            itemdisplay[e.Item.Index] = tempitem;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool itemchecked = false;
+            if (!string.IsNullOrEmpty(textBox6.Text))
+            {
+                DialogResult dialogResult = MessageBox.Show("Are you sure want to update", "Check ? ", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.No)
+                {
+                    return;
+                }
+                Test1.Utilities util = new Utilities();
+                Transaction transaction = new Transaction();
+                transaction.clientname = textBox3.Text;
+                transaction.clientid = Convert.ToInt32(textBox2.Text);
+                transaction.totalamount = Convert.ToSingle("-" + textBox6.Text);
+                transaction.date = "03072017";
+                transaction.sellreturn = 1;
+
+                foreach (itemdisp item in itemdisplay)
+                {
+                    if ((item.x == true))
+                    {
+                        itemchecked = true;
+                        transaction.items += item.codefinal + "&&++" + "-"+item.qtyfinal + "++&&";
+                        try
+                        {
+
+                            MessageBox.Show("Information updated");
+
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Failed");
+                        }
+                    }
+                }
+                if (itemchecked)
+                    util.InsertTranscation(transaction);
+                else
+                    MessageBox.Show("No items checked!");
+            }
+            else
+            {
+                MessageBox.Show("Please Insert a value ");
+            }
+        }
+
+
     }
 
 
-
 }
+
+
+
+
